f0edacd0b8cd6277be627f7e5b3c2c13f6b5254f
VladimirPolozov <57739258+VladimirPolozov@users.noreply.github.com>
2024-04-07 23:49:48 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,36 +8,12 @@ namespace BinaryTree
 {
     public class BinaryTree<T> : IEnumerable<T>  where T : IComparable
     {
-        public enum Side
-        {
-            Left,
-            Right
-        }
-
         public class Node
         {
             public T Value { get; set; }
             public Node Left { get; set; }
             public Node Right { get; set; }
             public Node ParentNode { get; set; }
-            public Node LeftNode { get; set; }
-            public Node RightNode { get; set; }
-            public Side? NodeSide
-            {
-                get
-                {
-                    if (ParentNode == null)
-                    {
-                        return null;
-                    } else if (ParentNode.LeftNode == this)
-                    {
-                        return Side.Left;
-                    } else
-                    {
-                        return Side.Right;
-                    }
-                }
-            }
 
             public Node(T value)
             {
@@ -58,7 +34,8 @@ namespace BinaryTree
             if (RootNode == null)
             {
                 RootNode = newNode;
-            } else
+            }
+            else
             {
                 AddRecursively(newNode, RootNode);
             }
@@ -74,21 +51,25 @@ namespace BinaryTree
                 {
                     currentNode.Left = newNode;
                     newNode.ParentNode = currentNode;
-                } else
+                }
+                else
                 {
                     return AddRecursively(newNode, currentNode.Left);
                 }
-            } else if (comparisonResult > 0)
+            }
+            else if (comparisonResult > 0)
             {
                 if (currentNode.Right == null)
                 {
                     currentNode.Right = newNode;
                     newNode.ParentNode = currentNode;
-                } else
+                }
+                else
                 {
                     return AddRecursively(newNode, currentNode.Right);
                 }
-            } else
+            }
+            else
             {
                 throw new InvalidOperationException("Узел с таким значением уже существует");
             }
@@ -107,21 +88,21 @@ namespace BinaryTree
 
         private class BinaryTreeEnumerator : IEnumerator<T>
         {
-            private Stack<Node> stack = new Stack<Node>();
-            private Node current;
+            private Stack<Node> _stack = new Stack<Node>();
+            private Node _current;
 
             public BinaryTreeEnumerator(Node root)
             {
-                current = root;
+                _current = root;
                 PushLeftNodes();
             }
 
             private void PushLeftNodes()
             {
-                while (current != null)
+                while (_current != null)
                 {
-                    stack.Push(current);
-                    current = current.Left;
+                    _stack.Push(_current);
+                    _current = _current.Left;
                 }
             }
 
@@ -129,23 +110,23 @@ namespace BinaryTree
             {
                 get
                 {
-                    if (stack.Count == 0)
+                    if (_stack.Count == 0)
                         return default; // Возвращаем значение по умолчанию, если стек пуст
-                    return stack.Peek().Value;
+                    return _stack.Peek().Value;
                 }
             }
             object IEnumerator.Current => Current;
 
             public bool MoveNext()
             {
-                if (stack.Count == 0)
+                if (_stack.Count == 0)
                     return false;
 
-                current = stack.Pop();
-                Node node = current.Right;
+                _current = _stack.Pop();
+                Node node = _current.Right;
                 while (node != null)
                 {
-                    stack.Push(node);
+                    _stack.Push(node);
                     node = node.Left;
                 }
                 return true;
@@ -157,32 +138,6 @@ namespace BinaryTree
             }
 
             public void Dispose() { }
-        }
-
-        private void PrintTree(Node startNode, string indent = "", Side? side = null)
-        {
-            if (startNode != null)
-            {
-                string nodeSide;
-                switch (side)
-                {
-                    case null:
-                        nodeSide = "+";
-                        break;
-                    case Side.Left:
-                        nodeSide = "L";
-                        break;
-                    default:
-                        nodeSide = "R";
-                        break;
-                }
-
-                Console.WriteLine($"{indent} [{nodeSide}] - {startNode.Value}");
-                indent += "   ";
-                // Рекурсивный вызов для левой и правой ветвей
-                PrintTree(startNode.Left, indent, Side.Left);
-                PrintTree(startNode.Right, indent, Side.Right);
-            }
         }
     }
 
